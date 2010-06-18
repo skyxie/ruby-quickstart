6591307a96f83b3f9f0e10954320206eaa815b3d
zuora <ryan.choi@zuora.com>
2010-06-18 09:32:27 -0700

    first pass

---
+++ app/controllers/sign_up_controller.rb
@@ -0,0 +1,19 @@
+class SignUpController < ApplicationController
+   def action
+      render :file => 'app\views\sign_up\signup.rhtml'
+   end
+   def list
+   end
+   def show
+   end
+   def new
+   end
+   def create
+   end
+   def edit
+   end
+   def update
+   end
+   def delete
+   end
+end
